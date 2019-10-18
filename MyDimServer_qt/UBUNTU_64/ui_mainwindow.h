47d1506661372e0ba8589aab9c9d869111194106
OK2100 <vseOk37@yande.ru>
2019-10-18 19:10:21 +0300

    Centos(RHEL) and Ubuntu folders added

---
+++ MyDimServer_qt/UBUNTU_64/ui_mainwindow.h
@@ -0,0 +1,154 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.13.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTextEdit>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QGridLayout *gridLayout_2;
+    QGridLayout *gridLayout;
+    QPushButton *setDnsButton;
+    QPushButton *setNameButton;
+    QPushButton *stoButton;
+    QLineEdit *lineEdit;
+    QPushButton *startbutton;
+    QLabel *label_2;
+    QLabel *label;
+    QLineEdit *lineEdit_2;
+    QTextEdit *textEdit;
+    QMenuBar *menubar;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->resize(400, 300);
+        QSizePolicy sizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(MainWindow->sizePolicy().hasHeightForWidth());
+        MainWindow->setSizePolicy(sizePolicy);
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        gridLayout_2 = new QGridLayout(centralwidget);
+        gridLayout_2->setObjectName(QString::fromUtf8("gridLayout_2"));
+        gridLayout = new QGridLayout();
+        gridLayout->setObjectName(QString::fromUtf8("gridLayout"));
+        setDnsButton = new QPushButton(centralwidget);
+        setDnsButton->setObjectName(QString::fromUtf8("setDnsButton"));
+        setDnsButton->setCheckable(true);
+        setDnsButton->setAutoDefault(false);
+        setDnsButton->setFlat(false);
+
+        gridLayout->addWidget(setDnsButton, 0, 2, 1, 1);
+
+        setNameButton = new QPushButton(centralwidget);
+        setNameButton->setObjectName(QString::fromUtf8("setNameButton"));
+        setNameButton->setCheckable(true);
+
+        gridLayout->addWidget(setNameButton, 1, 2, 1, 1);
+
+        stoButton = new QPushButton(centralwidget);
+        stoButton->setObjectName(QString::fromUtf8("stoButton"));
+
+        gridLayout->addWidget(stoButton, 3, 2, 1, 1);
+
+        lineEdit = new QLineEdit(centralwidget);
+        lineEdit->setObjectName(QString::fromUtf8("lineEdit"));
+
+        gridLayout->addWidget(lineEdit, 0, 1, 1, 1);
+
+        startbutton = new QPushButton(centralwidget);
+        startbutton->setObjectName(QString::fromUtf8("startbutton"));
+
+        gridLayout->addWidget(startbutton, 2, 2, 1, 1);
+
+        label_2 = new QLabel(centralwidget);
+        label_2->setObjectName(QString::fromUtf8("label_2"));
+
+        gridLayout->addWidget(label_2, 1, 0, 1, 1);
+
+        label = new QLabel(centralwidget);
+        label->setObjectName(QString::fromUtf8("label"));
+
+        gridLayout->addWidget(label, 0, 0, 1, 1);
+
+        lineEdit_2 = new QLineEdit(centralwidget);
+        lineEdit_2->setObjectName(QString::fromUtf8("lineEdit_2"));
+
+        gridLayout->addWidget(lineEdit_2, 1, 1, 1, 1);
+
+        textEdit = new QTextEdit(centralwidget);
+        textEdit->setObjectName(QString::fromUtf8("textEdit"));
+
+        gridLayout->addWidget(textEdit, 2, 0, 2, 2);
+
+
+        gridLayout_2->addLayout(gridLayout, 0, 0, 2, 1);
+
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 400, 22));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        MainWindow->setStatusBar(statusbar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        setDnsButton->setText(QCoreApplication::translate("MainWindow", "Apply", nullptr));
+        setNameButton->setText(QCoreApplication::translate("MainWindow", "Apply", nullptr));
+        stoButton->setText(QCoreApplication::translate("MainWindow", "Stop", nullptr));
+        lineEdit->setText(QCoreApplication::translate("MainWindow", "hplu-Notebook-PC", nullptr));
+        startbutton->setText(QCoreApplication::translate("MainWindow", "Start", nullptr));
+        label_2->setText(QCoreApplication::translate("MainWindow", "Set DIM server name:", nullptr));
+        label->setText(QCoreApplication::translate("MainWindow", "Set DNS node:", nullptr));
+        lineEdit_2->setText(QCoreApplication::translate("MainWindow", "FIT DIM server", nullptr));
+        textEdit->setHtml(QCoreApplication::translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:'Ubuntu'; font-size:11pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:'Ubuntu';\">Please start DNS server manually.</span></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:'Ubuntu';\"><br /></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:'Ubuntu';\">You can find it in 'dim_v20r26/bin32' or 'dim_v20r26/linux' folders.</span></p></body></html>", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
